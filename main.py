f7d919db7b974a23748886cf93ca21b039c69390
Eder Mateus Arcenego <edermateus6@unesc.net>
2025-07-08 10:04:47 -0300

    Revertendo a configuração do main.py.

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
 ﻿import streamlit as st
 
-st.set_page_config(
-    page_title="Narwave AI",
-    page_icon="🐋",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
-
-st.markdown("# 👋 Bem-vindo à Narwave AI")
-st.markdown("Selecione uma opção no menu lateral para começar.")
+st.switch_page("pages/0_Home.py")
